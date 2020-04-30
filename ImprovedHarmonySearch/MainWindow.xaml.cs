4073eb5b6556471925dc87d7227eeafbacd13793
wrkfmbb <kingapecherz@gmail.com>
2020-04-30 12:08:44 +0200

    double comma to dot

--- ImprovedHarmonySearch/MainWindow.xaml.cs
+++ ImprovedHarmonySearch/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using System.Threading;
+
+
 
 namespace ImprovedHarmonySearch
 {
@@ -21,9 +26,13 @@ namespace ImprovedHarmonySearch
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        System.IFormatProvider cultureUS = new System.Globalization.CultureInfo("en-US");
+        System.Globalization.CultureInfo cultureInfo = new System.Globalization.CultureInfo("en-US");
+
         Random rand = new Random(); // dla pewności rand musi być globalny (jesli się okaze ze nie wystepuje nigdzie ponowna 
         //jego instancja w innych klasach to mozna go przenisc do klasy IHS (Search Harmony) 
-        int variablesCount = new int(); 
+        int variablesCount;
         const int n = 5; //maksymalna liczba zmiennych z założeń
         string values; //joined values with "," 
         Function f; //funkcja celu 
@@ -39,6 +48,11 @@ namespace ImprovedHarmonySearch
         int NI;
         int HMS;
 
+        double[] xL;
+        double[] xU;
+        double[][] HM;
+        double[] fValue;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -46,31 +60,113 @@ namespace ImprovedHarmonySearch
 
         private void SearchHarmony(object sender, RoutedEventArgs e)
         {
-            // var value = objectiveFunction.Text;
-            // result.Text = value;
-            //double[] rndW = new double[6];
-            //double[] rndG = new double[6]; 
-            //for (int i = 0; i < 6; i ++)
-            //{
-            //                 rndG[i] = rand.NextDouble()*(2-(-2)) - 2; //nextdouble to z uniform 
-            //}
+            InitializeParameters();
+            HM = InitializeHM();
+            fValue = InitializeFxValue();
+            SortTwoArrays(HM, fValue); 
 
+        }
+
+        private void InitializeParameters()
+        {
             hmcr = double.Parse(HMCR.Text);
             parMIN = double.Parse(parMin.Text);
             parMAX = double.Parse(parMax.Text);
             bwMIN = double.Parse(bwMin.Text);
-            bwMIN = double.Parse(bwMax.Text);
+            bwMAX = double.Parse(bwMax.Text);
             NI = int.Parse(Ni.Text);
-            HMS = int.Parse(hms.Text); 
+            HMS = int.Parse(hms.Text);
+
+            //inicjalizacja lower and upper bound
+            //for (int i = 1; i < variablesCount + 1; i++)
+            //{
+            //    xL[i] = double.Parse(($"x{i}min.Text");
+            //    xU[i] = double.Parse((TextBox)$"x{i}max.Text");
+            //}  ----> to jest źle nie działa ale trzeba zautomatyzować proces zczytywania z textboxów 
+
+            xL = new double[]
+            {
+                double.Parse(x1min.Text),
+                double.Parse(x2min.Text),
+                // double.Parse(x3min.Text) itp dla n > 2 
+            };
+
+            xU = new double[]
+            {
+
+                double.Parse(x1max.Text),
+                double.Parse(x2max.Text),
+                // double.Parse(x3min.Text) itp dla n > 2 
+            };
         }
 
+        private void SortTwoArrays(double[][] HM, double[] fx)
+        {
+            for (int i = 0; i < HMS; i++)
+            {
+                Array.Sort(fx,HM[]);
+            }
+        }
+
+        private double[][] InitializeHM()
+        {
+            double[][] HM = new double[HMS][]; //+1 bo jeszcze wartosc funkcji celu  
+
+            for (int z = 0; z < HMS; z++)
+            {
+                HM[z] = new double[variablesCount];
+            }
+
+            for (int j = 0; j < variablesCount; j++)
+            {
+                for (int i = 0; i < HMS; i++) //najpierw inicjalizacja dla całego x1, później dla x2, bo zakresy moga sie roznic 
+                {
+                    HM[i][j] = RandomNumberInScope(xL[j], xU[j]);
+                }
+            }
+
+            return HM;
+        }
+
+        private double RandomNumberInScope(double lowerBound, double upperBound)
+        {
+            double random = (rand.NextDouble() * (upperBound - lowerBound)) + lowerBound; //nextdouble to z uniform [0,1]  
+
+            return random;
+        }
+
+
+        // xArr to tablica wartości zmiennych x1,x2 ... 
+        private double CalculateObjectiveFunc(double[] xArr)
+        {
+            double result;
+
+            string spr = string.Join(",", xArr);
+            ex = new org.mariuszgromada.math.mxparser.Expression($"f({string.Join(",", xArr)})", f);
+            result = ex.calculate();
+
+            return result;
+        }
+
+        private double[] InitializeFxValue()
+        {
+            double[] fValue = new double[HMS];
+
+            for (int i = 0; i < HMS; i++)
+            {
+                fValue[i] = CalculateObjectiveFunc(HM[i]);
+            }
+
+            return fValue;
+
+        }
         private void lostFocusOnObjFunc(object sender, RoutedEventArgs e)
         {
             var expression = objectiveFunction.Text;
             detectedVar.Text = string.Empty;
             variableNames = new List<string>(); //list of variables name 
             XScopeWindow window = new XScopeWindow();
-            
+
             //wyszukanie liczby zmiennych n <= 5 z załozenia od pani Doktor 
             for (int i = 1; i < n + 1; i++)
             {
@@ -80,31 +176,22 @@ namespace ImprovedHarmonySearch
                     variableNames.Add($"x{i}");
                 }
             }
-                       
+
             values = string.Join(",", variableNames);
             detectedVar.Text = values;
 
             f = new Function($"f({values}) = {expression}");
-            double[] db = new double[2] { 1,5};
+            double[] db = new double[2] { 1, 5 };
 
             result.Text = $"{CalculateObjectiveFunc(db)}";
 
-           
+
             window.Show();
-            
+
         }
 
-
-        // xArr to tablica wartości zmiennych x1,x2 ... 
-        private double CalculateObjectiveFunc(double[] xArr)
-        {
-            double result;
-
-            ex = new org.mariuszgromada.math.mxparser.Expression($"f({string.Join(",", xArr)})", f);
-            result = ex.calculate();
-
-            return result;
-        }
 
     }
+
 }
+
--- ImprovedHarmonySearch/XScopeWindow.xaml.cs
+++ ImprovedHarmonySearch/XScopeWindow.xaml.cs
@@ -25,5 +25,10 @@ namespace ImprovedHarmonySearch
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        { 
+            this.Close(); 
+        }
     }
 }
